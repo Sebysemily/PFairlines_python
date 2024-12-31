cbbfd739d6b23bc2ddad952e536177a2104f96b5
Roger <sebascalvas@outlook.com>
2024-12-31 03:52:47 -0500

    plane.py implemented

---
+++ plane.py
@@ -0,0 +1,64 @@
+from operator import truediv
+
+
+class Plane:
+    """Represents a plane"""
+    def __init__(self, plane_id, name, rows=5, columns=("A", "B", "C", "D")):
+        self.plane_id = plane_id
+        self.name = name
+        self.rows = rows
+        self.columns = columns
+        self.seats = {}
+
+
+    def initialize_seats(self, day):
+        for row in range(1, self.rows + 1):
+            for column in self.columns:
+                seat = f"{column}{row}}"
+                insert_seat(self.plane_id, seat, day)
+
+
+    def load_seats(self, day):
+        self.seats = {}
+        query = """
+        SELECT seat, resereved
+        from seats
+        WHERE plane_id = %s AND day = %s;
+        """
+        seat_data = execute_query(query, (self.plane_id, day))
+        if seat_data:
+            for seat, reserved in seat_data:
+                self.seats[seat] = reserved
+
+
+
+    def display_seats(self):
+        print(f"\nSeating arrangement for {self.name}:")
+        print("    "+" ".join(self.columns))
+        for row in range(1, self.rows+1):
+            row_display = []
+            for column in self.columns:
+                seat = f"{column}{row}"
+                row_display.append("X" if self.seats.get(seat, False) else "*")
+            print(f"Row {row:<2}: " + " ".join(row_display))
+
+
+
+    def reserve_seat(self, seat, day):
+        if seat not in self.seats:
+            print(f"Seat {seat} does not exist in this plane. ")
+            return False
+        if self.seats[seat]:
+            print(f"Seat {seat} is already reserved.")
+            return False
+        success = reserve_seat(self.plane_id, seat, day)
+        if success:
+            self.seats[seat] = true
+            print(f"Seat {seat} has been succesfully reserved.")
+            return True
+        else:
+            print(f"Failed to reserve seat {seat}.")
+            return False
+
+    def get_available_seats(self, day):
+        return get_available_seats(self.plane_id, day)
---
+++ utils/db.py
@@ -0,0 +1,3 @@
+from psycopg2 import sql
+def test_db():
+    create_tables()
