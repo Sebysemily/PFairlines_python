6e3c22b9e8f456b38b035b67d6288aff706c24d1
Roger <sebascalvas@outlook.com>
2024-12-31 19:36:09 -0500

    Added db_test.py for pstgreSQL testing

--- utils/db.py
+++ utils/db.py
@@ -1,3 +1,9 @@
+import psycopg2
 from psycopg2 import sql
-def test_db():
-    create_tables()
+
+    conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres",
+                            password="Sebi3980", port="5432")
+    cur = conn.cursor()
+
+    conn.commit()
+    
